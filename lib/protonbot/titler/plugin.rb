4249d2bfd2ee2c6b904be6bed2cb9266f85b51d6
Nickolay <nickolay02@inbox.ru>
2017-03-31 22:53:56 +0300

    Small fix to prevent detecting URLs without schema-prefix

--- lib/protonbot/titler/plugin.rb
+++ lib/protonbot/titler/plugin.rb
@@ -9,7 +9,7 @@ ProtonBot::Plugin.new do
   hook(type: :privmsg) do |dat|
     #core.privmsg_patch(dat)
     unless /(bot|serv)/i.match("#{dat[:nick]}!#{dat[:user]}@#{dat[:host]}")
-      regex = %r((?:(?:https?):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)i
+      regex = %r((?:(?:https?):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)i
       pattern = "%N%B[ :NICK%N#:URLNUM %B|%N :OUT %B]%N"
       dat[:message].scan(regex)[0,5].each_with_index do |v, k|
         unless %r(http[s]?://).match(v)
