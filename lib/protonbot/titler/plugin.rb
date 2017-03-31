0062695aac03c908631830be856777b1e5438c33
Nickolay <nickolay02@inbox.ru>
2017-03-31 23:19:25 +0300

    No newlines now. At least AFAIK

--- lib/protonbot/titler/plugin.rb
+++ lib/protonbot/titler/plugin.rb
@@ -36,7 +36,7 @@ ProtonBot::Plugin.new do
           dat.reply(pattern
             .gsub(':NICK', dat[:nick])
             .gsub(':URLNUM', (k+1).to_s)
-            .gsub(':OUT', "%C%GREEN\u200B" + html.title + '%N'))
+            .gsub(':OUT', "%C%GREEN\u200B" + html.title.strip.gsub(/\n/, ' ') + '%N'))
         else
           dat.reply(pattern
             .gsub(':NICK', dat[:nick])
